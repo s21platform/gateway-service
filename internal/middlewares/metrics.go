ab17aac578f38ab9dbadc19d37c63509d561e166
asepopov <asepopov@avito.ru>
2024-09-24 20:20:55 +0300

    add middleware with metrics

--- internal/middlewares/metrics.go
+++ internal/middlewares/metrics.go
@@ -9,7 +9,7 @@ import (
 
 func MetricMiddleware(next http.Handler, metrics *pkg.Metrics) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		metrics.Increment(r.URL.Path)
+		metrics.Increment("auth.login")
 		log.Println("Increment for", r.URL.Path)
 		ctx := context.WithValue(r.Context(), "metrics", metrics)
 		next.ServeHTTP(w, r.WithContext(ctx))

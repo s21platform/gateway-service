b0aff2c86c7ff63307fc6939f41d3f49c6d40c35
alex <test@MacBook-Air-test.local>
2024-12-26 19:54:22 +0300

    added func GetPermission

--- internal/handlers/api/api.go
+++ internal/handlers/api/api.go
@@ -477,7 +477,6 @@ func (h *Handler) GetPermission(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write(jsn)
-
 }
 
 func AttachApiRoutes(r chi.Router, handler *Handler, cfg *config.Config) {

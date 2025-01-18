1a90c995bc24af624b80601d39d617ecf460a5bd
mikhail-kalugin <Alekseevich.m.k@gmail.com>
2025-01-18 15:44:56 +0300

    change chat useCase

--- internal/useCase/chat/chat.go
+++ internal/useCase/chat/chat.go
@@ -1,11 +1,12 @@
 package chat
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	chat "github.com/s21platform/chat-proto/chat-proto"
-	"github.com/s21platform/gateway-service/internal/config"
 )
 
 type Usecase struct {
@@ -17,9 +18,25 @@ func New(cC ChatClient) *Usecase {
 }
 
 func (u *Usecase) GetRecentMessages(r *http.Request) (*chat.GetRecentMessagesOut, error) {
-	uuid := r.Context().Value(config.KeyUUID).(string)
+	var requestData struct {
+		Uuid string `json:"uuid"`
+	}
 
-	resp, err := u.cC.GetRecentMessages(r.Context(), uuid)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read request body: %v", err)
+	}
+	defer r.Body.Close()
+
+	if len(body) == 0 {
+		return nil, fmt.Errorf("request body is empty")
+	}
+
+	if err = json.Unmarshal(body, &requestData); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal request body: %v", err)
+	}
+
+	resp, err := u.cC.GetRecentMessages(r.Context(), requestData.Uuid)
 	if err != nil {
 		return nil, fmt.Errorf("failed to u.cC.GetRecentMessages: %v", err)
 	}

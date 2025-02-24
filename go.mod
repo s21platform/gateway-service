de456846ccc5645d703b6deb1d47d8f42bf8e614
njohanne <aleksey88.avt@gmail.com>
2025-02-24 18:55:39 +0300

    STY-49_CreateSociety

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.8
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/golang-jwt/jwt/v4 v4.5.1
+	github.com/golang/mock v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/s21platform/auth-proto v0.0.0-20240514091002-319cbc4745e5
 	github.com/s21platform/avatar-proto v0.0.10
@@ -15,8 +16,9 @@ require (
 	github.com/s21platform/notification-proto v0.0.2
 	github.com/s21platform/optionhub-proto v1.0.6
 	github.com/s21platform/search-proto v0.0.10
-	github.com/s21platform/society-proto v0.0.16
+	github.com/s21platform/society-proto v0.0.21
 	github.com/s21platform/user-proto v0.0.11
+	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.69.2
 )
 
@@ -24,12 +26,10 @@ require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/alexcesaro/statsd v2.0.0+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
--- internal/handlers/api/api_test.go
+++ internal/handlers/api/api_test.go
@@ -7,8 +7,6 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	society_proto "github.com/s21platform/society-proto/society-proto"
-
 	"github.com/golang/mock/gomock"
 	"github.com/s21platform/gateway-service/internal/config"
 	logger_lib "github.com/s21platform/logger-lib"
@@ -82,81 +80,6 @@ func TestApi_GetProfile(t *testing.T) {
 		)
 
 		s.MyProfile(w, r)
-
-		assert.Equal(t, http.StatusInternalServerError, w.Code)
-	})
-}
-
-func TestApi_GetSocietyInfo(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-
-	ctx := context.Background()
-	mockLogger := logger_lib.NewMockLoggerInterface(ctrl)
-
-	t.Run("should_ok", func(t *testing.T) {
-		MockSocietyService := NewMockSocietyService(ctrl)
-		mockLogger.EXPECT().AddFuncName("SocietyInfo")
-
-		ctx = context.WithValue(ctx, config.KeyLogger, mockLogger)
-		r := &http.Request{}
-		w := httptest.NewRecorder()
-		r = r.WithContext(ctx)
-
-		societyInfo := &society_proto.GetSocietyInfoOut{
-			Name:             "test",
-			Description:      "test",
-			OwnerUUID:        "test",
-			PhotoUrl:         "test",
-			IsPrivate:        true,
-			CountSubscribers: 0,
-		}
-
-		MockSocietyService.EXPECT().GetSocietyInfo(r).Return(societyInfo, nil)
-
-		s := New(
-			nil,
-			nil,
-			nil,
-			nil,
-			nil,
-			MockSocietyService,
-			nil,
-			nil,
-		)
-
-		s.GetSocietyInfo(w, r)
-
-		assert.Equal(t, http.StatusOK, w.Code)
-	})
-
-	t.Run("should_err_us_fail_response", func(t *testing.T) {
-		MockSocietyService := NewMockSocietyService(ctrl)
-		mockLogger.EXPECT().AddFuncName("SocietyInfo")
-		mockLogger.EXPECT().Error(gomock.Any())
-
-		ctx = context.WithValue(ctx, config.KeyLogger, mockLogger)
-		r := &http.Request{}
-		w := httptest.NewRecorder()
-		r = r.WithContext(ctx)
-
-		mockErr := errors.New("some error")
-
-		MockSocietyService.EXPECT().GetSocietyInfo(r).Return(nil, mockErr)
-
-		s := New(
-			nil,
-			nil,
-			nil,
-			nil,
-			nil,
-			MockSocietyService,
-			nil,
-			nil,
-		)
-
-		s.GetSocietyInfo(w, r)
 
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})

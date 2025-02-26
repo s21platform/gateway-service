2e3a85f06c40ff4abddb52c6e1ed80b443d0e27d
Tony <akatkov89@mail.ru>
2025-02-26 22:39:49 +0300

    argument name fix in advert\contract.go

--- internal/useCase/advert/contract.go
+++ internal/useCase/advert/contract.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	advert "github.com/s21platform/advert-proto/advert-proto"
+
+	"github.com/s21platform/gateway-service/internal/model"
 )
 
 type AdvertClient interface {
 	GetAdverts(ctx context.Context, uuid string) (*advert.GetAdvertsOut, error)
-	CreateAdvert(ctx context.Context, req *RequestData) (*advert.AdvertEmpty, error)
+	CreateAdvert(ctx context.Context, req *model.AdvertRequestData) (*advert.AdvertEmpty, error)
 }
